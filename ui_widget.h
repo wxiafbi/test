7b49aa5c9cb43467236b87c11b2f47e9ffc2976a
wxiafbi <71679226+wxiafbi@users.noreply.github.com>
2022-12-13 15:54:56 +0800

    first commit

---
+++ ui_widget.h
@@ -0,0 +1,84 @@
+/********************************************************************************
+** Form generated from reading UI file 'widgetKkaXMQ.ui'
+**
+** Created by: Qt User Interface Compiler version 6.3.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef WIDGETKKAXMQ_H
+#define WIDGETKKAXMQ_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QLCDNumber>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Widget
+{
+public:
+    QLCDNumber *lcdNumber;
+    QWidget *widget;
+    QVBoxLayout *verticalLayout;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton_3;
+
+    void setupUi(QWidget *Widget)
+    {
+        if (Widget->objectName().isEmpty())
+            Widget->setObjectName(QString::fromUtf8("Widget"));
+        Widget->resize(800, 600);
+        lcdNumber = new QLCDNumber(Widget);
+        lcdNumber->setObjectName(QString::fromUtf8("lcdNumber"));
+        lcdNumber->setGeometry(QRect(450, 200, 151, 71));
+        widget = new QWidget(Widget);
+        widget->setObjectName(QString::fromUtf8("widget"));
+        widget->setGeometry(QRect(130, 40, 77, 401));
+        verticalLayout = new QVBoxLayout(widget);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        verticalLayout->setContentsMargins(0, 0, 0, 0);
+        pushButton = new QPushButton(widget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+
+        verticalLayout->addWidget(pushButton);
+
+        pushButton_2 = new QPushButton(widget);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+
+        verticalLayout->addWidget(pushButton_2);
+
+        pushButton_3 = new QPushButton(widget);
+        pushButton_3->setObjectName(QString::fromUtf8("pushButton_3"));
+
+        verticalLayout->addWidget(pushButton_3);
+
+
+        retranslateUi(Widget);
+        QObject::connect(pushButton, &QPushButton::pressed, lcdNumber, qOverload<>(&QLCDNumber::close));
+        QObject::connect(pushButton, &QPushButton::released, lcdNumber, qOverload<>(&QLCDNumber::show));
+
+        QMetaObject::connectSlotsByName(Widget);
+    } // setupUi
+
+    void retranslateUi(QWidget *Widget)
+    {
+        Widget->setWindowTitle(QCoreApplication::translate("Widget", "Widget", nullptr));
+        pushButton->setText(QCoreApplication::translate("Widget", "PushButton", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("Widget", "\350\207\252\345\242\236", nullptr));
+        pushButton_3->setText(QCoreApplication::translate("Widget", "\350\207\252\345\207\217", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Widget: public Ui_Widget {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // WIDGETKKAXMQ_H
